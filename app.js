343c368b19c6d7a0ded09d8f8e8acaa0f1580904
engdavide <twodoorbells@gmail.com>
2018-10-25 17:13:51 +0000

    add initial partials and first post route

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+    var books = [
+        {name: "book1", description: "words about book1"},
+        {name: "book2", description: "words about book3"},
+        {name: "book2", description: "words about bookk3"},
+    ];
+
+
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
@@ -8,14 +19,22 @@ app.get("/", function(req, res){
 });
 
 app.get("/books", function(req,res){
-    var books = [
-        {name: "book1", description: "words about book1"},
-        {name: "book2", description: "words about book3"},
-        {name: "book2", description: "words about bookk3"},
-    ]
+
     res.render("books", {books:books});
 });
 
+app.get("/books/new", function(req,res){
+    res.render("new.ejs")
+})
+
+app.post("/books", function(req, res){
+    var name = req.body.name;
+    var description = req.body.description;
+    var newBook = {name: name, description: description};
+    books.push(newBook)
+    res.redirect("books")
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("bookshare is up!!")
+    console.log("bookshare is up!!");
 })
