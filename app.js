b8b931eb677a567c34788d0c0aaaefa836485fca
EC2 Default User <ec2-user@ip-172-31-25-15.us-east-2.compute.internal>
2018-10-25 16:29:19 +0000

    build initial pages and routing

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/books", function(req,res){
+    var books = [
+        {name: "book1", description: "words about book1"},
+        {name: "book2", description: "words about book3"},
+        {name: "book2", description: "words about bookk3"},
+    ]
+    res.render("books", {books:books});
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("bookshare is up!!")
 })
